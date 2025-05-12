5975f940bd7405cfbc803f5d0d0eb0dfabbddb61
ouassari <hannae.ouassari@gmail.com>
2025-05-12 10:42:30 +0200

    begin of card

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,25 @@ function App() {
 
   return (
     <>
+    <div className='df'>
+
+<div className='card'>
+
+    <div className={"cardd " + props.info.title.toLowerCase()}>
     
+            <div className='dex'>
+                <p className='title'>{props.info.title}  </p>
+                <img className='punt' src={punt} alt="" />
+            </div>
+            <div className='tecard'>
+                <h1 className='current'>{props.current} hrs </h1>
+                <p className='previous'> {text} - {props.previous} hrs  </p>
+            </div>
+
+    </div>
+</div>
+
+</div>
     </>
   )
 }
